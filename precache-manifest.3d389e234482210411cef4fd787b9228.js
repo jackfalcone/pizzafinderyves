90858bae1bc15872f533ff735dc574b3067cb7ca
jackfalcone <yves.daetwyler@gmail.com>
2020-07-12 02:30:44 +0200

    Build 01 ready for testing

---
+++ precache-manifest.3d389e234482210411cef4fd787b9228.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40886d4aa80f18276ffac33da12600db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00b613bc49fa8c8038dc",
+    "url": "/static/css/main.ea13b92a.chunk.css"
+  },
+  {
+    "revision": "40a536335e184874c60b",
+    "url": "/static/js/2.ff10b991.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ff10b991.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00b613bc49fa8c8038dc",
+    "url": "/static/js/main.73a3b54a.chunk.js"
+  },
+  {
+    "revision": "0b7eb9d469061bce3505",
+    "url": "/static/js/runtime-main.140093ed.js"
+  },
+  {
+    "revision": "0d12d92b68402a14c77fc7c6354f912d",
+    "url": "/static/media/background_search_desktop.0d12d92b.jpg"
+  },
+  {
+    "revision": "27edc6b2530eebf58989201501859451",
+    "url": "/static/media/background_search_mobile.27edc6b2.jpg"
+  }
+]);
